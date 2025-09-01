9c636f3259d836e96505c9a48ca82765faa1596c
Maksym Kulakov <maksym.kulakov@ica.de>
2025-09-01 13:43:26 +0200

    Error fixing

--- task.sql
+++ task.sql
@@ -1,5 +1,5 @@
 -- write your code for database user creation here
-create user if not exists 'webappuser'@'localhost' identified by 'P@ssw0rd';
-grant select, update, delete, insert on ShopDB.* to 'webappuser'@'localhost';
-create user if not exists 'deploymentuser'@'localhost' identified by 'P@ssw0rd';
-grant all privileges on ShopDB.* to 'deploymentuser'@'localhost';
+create user if not exists 'webappuser'@'%' identified by 'P@ssw0rd';
+grant select, update, delete, insert on ShopDB.* to 'webappuser'@'%';
+create user if not exists 'deploymentuser'@'%' identified by 'P@ssw0rd';
+grant all privileges on ShopDB.* to 'deploymentuser'@'%';
