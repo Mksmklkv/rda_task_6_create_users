0f777958775a3cbdc1ba86a8e284d600ccaf0182
Maksym Kulakov <maksym.kulakov@ica.de>
2025-09-01 13:29:53 +0200

    Task done

--- task.sql
+++ task.sql
@@ -1,1 +1,5 @@
 -- write your code for database user creation here
+create user 'webappuser'@'%' identified by 'P@ssw0rd';
+grant select, update, delete, insert on ShopDB.* to 'webappuser'@'%';
+create user 'deploymentuser'@'%' identified by 'P@ssw0rd';
+grant select, insert, update, delete, create on ShopDB.* to 'deploymentuser'@'%';
